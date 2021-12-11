64c29b90a9820c86458e686f8734cc1452135dcc
luisbull <luisbull@hotmail.com>
2021-12-11 15:35:13 +0000

    Extended Tailwind colors pallets.
    Not all colors come by default with this version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-    mode: 'jit',
+    // mode: 'jit',
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -14,6 +15,11 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                orange: colors.orange,
+                violet: colors.violet,
+                teal: colors.teal,
+            }
         },
         container: {
             center: true
