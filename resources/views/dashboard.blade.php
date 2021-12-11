8a5ca1e1b9a4f775f19638cda90507c0149f02e4
luisbull <luisbull@hotmail.com>
2021-12-11 17:37:55 +0000

    deleted default and added different content.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Coders
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cupiditate adipisci magni sint facilis, iure omnis illum repellendus quisquam voluptates modi eius minima ut cumque, excepturi, dicta nihil dolorem harum!</p>
+                <p>Esse qui perspiciatis possimus, dolorem pariatur amet nobis deserunt placeat natus laborum atque aperiam rerum est tempora, quis vero dolore repellat praesentium, odit eos doloribus asperiores totam optio! Amet, in!</p>
+                <p>Aut id modi omnis in iure, sed molestias architecto atque odit quas, natus perferendis vero animi error itaque quam debitis earum fugit, quasi veniam adipisci facere. Autem omnis perspiciatis quasi?</p>
+                <p>Iusto illum numquam modi magnam rem eius enim esse eum fugit officia! Cum assumenda doloremque, voluptas corporis in eaque impedit ab a. Voluptate quidem assumenda fugiat delectus minima dignissimos similique?</p>
+                <p>Sit, atque voluptatem autem culpa iure exercitationem minus obcaecati non quibusdam. Fugit at fuga odit sed quaerat quo alias atque, illo aut adipisci, quibusdam dolore earum vero nam velit debitis?</p>
             </div>
         </div>
     </div>
